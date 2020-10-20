c7891c1eab270f77862b9a86e00acc3dae90904b
Khuaja Mohammad Shams <ksham004@wch136-22.cs.ucr.edu>
2020-10-20 00:04:31 -0700

    source file for lab2 ex 2

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -1,7 +1,7 @@
 /*	Author: ksham004
  *  Partner(s) Name: Khuaja Shams
  *	Lab Section: 023
- *	Assignment: Lab #2  Exercise #1
+ *	Assignment: Lab #2  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -15,27 +15,26 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0x00; // Configure port A's 8 pins as inputs
-	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
-	unsigned char tmpA0 = 0x00; // Temporary variable to hold the value of A
-	unsigned char tmpA1 = 0x00;
+	DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	unsigned char tmpC = 0x00; // Temporary variable to hold the value of B
+	unsigned char tmpA = 0x00;
    /* Insert your solution below */
     while (1) {
 	// 1) Read input
-	tmpA0 = PINA & 0x01;
-	tmpA1 = PINA & 0x02;
+	tmpA = PORTA & 0x0F;
+	unsigned char cntavail = 0x00;
 	// 2) Perform computation
-	// if PA0 is 1, set PB1PB0 = 01, else = 10
-	 
-	if (tmpA0 && !tmpA1) { // True if PA0 is 1
-		tmpB = 1; // Sets tmpB to bbbbbb01
-					 // (clear rightmost 2 bits, then set to 01)
-	} else {
-		tmpB = 0; // Sets tmpB to bbbbbb10
-					 // (clear rightmost 2 bits, then set to 10)
+	unsigned int i = 0;
+	while (i < 4) {
+		if ((tmpA & 0x01) == 0x01) {
+			cntavail = cntavail + 1;
+		}
+		tmpA = tmpA >> 1;
+		i++;
 	}
+	tmpC = cntavail;
 	// 3) Write output
-	 PORTB = tmpB;		
+	PORTC = tmpC;		
     }
     return 0;
 }
