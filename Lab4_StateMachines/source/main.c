c1797062411ef99407fc8904c9307683ee0ac20a
Khuaja Mohammad Shams <ksham004@wch136-22.cs.ucr.edu>
2020-10-25 21:54:30 -0700

    initial exercise 3 commit

--- Lab4_StateMachines/source/main.c
+++ Lab4_StateMachines/source/main.c
@@ -12,122 +12,109 @@
 #include "simAVRHeader.h"
 #endif
 
+unsigned char tmpA = 0x00;
 unsigned char tmpA0 = 0x00;
 unsigned char tmpA1 = 0x00;
-unsigned char tmpC = 0x00;
+unsigned char tmpA2 = 0x00;
+unsigned char tmpA7 = 0x00;
+unsigned char tmpB = 0x00;
 
-enum SM1_STATES { SM1_SMStart, SM1_Init, SM1_Wait, SM1_Press0, SM1_Press1, SM1_Reset} SM1_STATE;
+
+enum SM1_STATES { SM1_SMStart, SM1_Lock, SM1_PressA2, SM1_PressA1, SM1_Open} SM1_STATE;
 void Tick_LoHi() { 
    switch(SM1_STATE) { 
       case SM1_SMStart:
          if (1) {
-            SM1_STATE = SM1_Init;
+            SM1_STATE = SM1_Lock;
          }
          break;
-      case SM1_Init:
-         if (1) {
-            SM1_STATE = SM1_Wait;
+      case SM1_Lock:
+         if ((tmpA0 || tmpA1 || tmpA7)) {
+            SM1_STATE = SM1_Lock;
          }
-         break;
-      case SM1_Wait:
-         if (!tmpA0 && !tmpA1) {
-            SM1_STATE = SM1_Wait;
-         }
-         else if (tmpA0 && !tmpA1) {
-			if (tmpC < 9) {
-				tmpC = tmpC + 1;
-			}
-            SM1_STATE = SM1_Press0;
-         }
-         else if (tmpA0 && tmpA1) {
-            SM1_STATE = SM1_Reset;
-         }
-         else if (!tmpA0 && tmpA1) {
-			if (tmpC > 0) {
-				tmpC = tmpC - 1;
-			}
-            SM1_STATE = SM1_Press1;
+         else if (tmpA2) {
+            SM1_STATE = SM1_PressA2;
          }
          else {
-            SM1_STATE = SM1_Wait;
+            SM1_STATE = SM1_Lock;
          }
          break;
-      case SM1_Press0:
-         if (tmpA0 && !tmpA1) {
-            SM1_STATE = SM1_Press0;
+      case SM1_PressA2:
+         if (!tmpA2) {
+            SM1_STATE = SM1_PressA1;
          }
-         else if (!tmpA0 && !tmpA1) {
-            SM1_STATE = SM1_Wait;
+         else if (tmpA2) {
+            SM1_STATE = SM1_PressA2;
          }
-         else if (tmpA0 && tmpA1) {
-            SM1_STATE = SM1_Reset;
-         }
-         else if (!tmpA0 && tmpA1) {
-            SM1_STATE = SM1_Press1;
+         else if ((tmpA0 || tmpA1 || tmpA7)) {
+            SM1_STATE = SM1_Lock;
          }
          else {
-            SM1_STATE = SM1_Press0;
+            SM1_STATE = SM1_PressA2;
          }
          break;
-      case SM1_Press1:
-         if (!tmpA0 && tmpA1) {
-            SM1_STATE = SM1_Press1;
+      case SM1_PressA1:
+         if ((tmpA0 || tmpA2 || tmpA7)) {
+            SM1_STATE = SM1_Lock;
+         } 
+		 else if (tmpA1) {
+            SM1_STATE = SM1_Open;
          }
-         else if (tmpA0 && tmpA1) {
-            SM1_STATE = SM1_Reset;
+         else if (!tmpA) {
+            SM1_STATE = SM1_PressA1;
          }
-         else if (tmpA0 && !tmpA1) {
-            SM1_STATE = SM1_Press0;
-         }
-         else if (!tmpA0 && !tmpA1) {
-            SM1_STATE = SM1_Wait;
-	    }
          else {
-            SM1_STATE = SM1_Press1;
+            SM1_STATE = SM1_PressA1;
          }
          break;
-      case SM1_Reset:
-         if (1) {
-            SM1_STATE = SM1_Wait;
+      case SM1_Open:
+         if (tmpA7) {
+            SM1_STATE = SM1_Lock;
+         }
+         else if (!tmpA7) {
+            SM1_STATE = SM1_Open;
+         }
+         else {
+            SM1_STATE = SM1_Open;
          }
          break;
       default:
-         SM1_STATE = SM1_Init;
+         SM1_STATE = SM1_Lock;
          break;
    }
    switch(SM1_STATE) { 
       case SM1_SMStart:
          
          break;
-      case SM1_Init:
-         tmpC = 7;
+      case SM1_Lock:
+         tmpB = 0;
          break;
-      case SM1_Wait:
+      case SM1_PressA2:
          
          break;
-      case SM1_Press0:
+      case SM1_PressA1:
          
          break;
-      case SM1_Press1:
-         
-         break;
-      case SM1_Reset:
-         tmpC = 0;
+      case SM1_Open:
+         tmpB = 1;
          break;
    }
 }
 
 int main(){
-    /* Insert DDR and PORT initializations */
-    DDRA = 0x00; PORTA = 0x00;
-    DDRC = 0xFF; PORTC = 0x00;
-    /* Insert your solution below */
-    while (1) {
+	DDRA = 0x00; PORTA = 0x00;
+    DDRB = 0xFF; PORTB = 0x00;
+   while(1){
+   	    tmpA = PINA;
 		tmpA0 = PINA & 0x01;
 		tmpA1 = PINA & 0x02;
+		tmpA2 = PINA & 0x04;
+		tmpA7 = PINA & 0x80;
+		
 		Tick_LoHi();	
-		PORTC = tmpC;
-    }
-    return 1;
+		
+		PORTB = tmpB;
+   }
+   return 1;
 }
 
